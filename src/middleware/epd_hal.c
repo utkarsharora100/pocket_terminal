d065f0668c02d65b2727afad7acabe2863df485a
utkarsharora100 <utkarsharora100@gmail.com>
2026-05-31 14:17:33 +0530

    Fixing the landscape loading

--- src/middleware/epd_hal.c
+++ src/middleware/epd_hal.c
@@ -6,12 +6,13 @@
 #include "GT1151.h"
 #include "touch_hal.h"
 /* string.h allows us to use memset for clearing the framebuffer */
-static uint8_t epd_buf[250*122/8];
-static int partial_refresh_counter = 0;
+static uint8_t epd_buf[16*250];
+static int partial_refresh_counter = 5;
 extern int IIC_Address;
 void epd_hal_init(){
  IIC_Address = 0x14;
  DEV_ModuleInit();
+ memset(epd_buf, 0xFF, sizeof(epd_buf));
  pthread_create(&touch_thread, NULL, touch_irq_thread, NULL);
  EPD_2in13_V4_Init(EPD_2IN13_V4_FULL);
  EPD_2in13_V4_Clear();
@@ -31,25 +32,41 @@ waveshare needs 1 bit per pixel , 8 pixels are packed into 1 bytes */
 /* 250 pixels / 8 pixels per byte = 31.25 bytes that is 32 */
 /* Eink uses MSB therefore we start from 7th index of the byte sequence */
 void epd_hal_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
-{
-  for (int y=area->y1;y<=area->y2;y++){
-		for(int x = area->x1;x<=area->x2;x++){
-			int byte_index = y *32 + x/8;
-			int bit_pos = 7 - (x%8);
+{  
+   printf("flush called: x1=%d y1=%d x2=%d y2=%d\n", area->x1, area->y1, area->x2, area->y2);
+   int stride = (16);  // LVGL actual stride = 32 bytes
+   int w = area->x2 - area->x1 + 1;
 
-			if (*px_map!=0x00){
-					epd_buf[byte_index] |= (1 << bit_pos);
-				}
-			else{
-				epd_buf[byte_index] &= ~( 1<< bit_pos);
-				}
-			px_map++;
-		}
-	}
+    for (int y = area->y1; y <= area->y2; y++) {
+        for (int x = area->x1; x <= area->x2; x++) {
+            if (x >= 250) continue;
+        
+        int local_x = x - area->x1;
+        int local_y = y - area->y1;
 
+        int src_byte = local_y * stride + local_x / 8;
+        int src_bit  = 7 - (local_x % 8);
+        int pixel    = (px_map[src_byte] >> src_bit) & 1;
+
+        int dst_byte = y * 16 + x / 8;
+        int dst_bit  = 7 - (x % 8);
+
+        if (pixel)
+            epd_buf[dst_byte] &=  ~(1 << dst_bit);
+        else
+            epd_buf[dst_byte] |= (1 << dst_bit);
+    }
+}
+       // count dark pixels
+        int dark = 0;
+        for (int i = 0; i < 32 * 122; i++) {
+           if (epd_buf[i] != 0xFF) dark++;
+        }
+        printf("dark bytes in buf: %d\n", dark);
 	if (lv_display_flush_is_last(disp))
 	{
 		partial_refresh_counter++;
+                printf("pushing to display, counter=%d\n", partial_refresh_counter);
 
 		if (partial_refresh_counter >=5)
 			{
@@ -61,7 +78,7 @@ void epd_hal_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 		else
 		{
 			EPD_2in13_V4_Init(EPD_2IN13_V4_PART);
-			EPD_2in13_V4_Display_Partial(epd_buf);
+			EPD_2in13_V4_Display_Partial_Wait(epd_buf);
 			EPD_2in13_V4_Sleep();
 		}
 	}
@@ -70,7 +87,7 @@ void epd_hal_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 lv_display_t* epd_hal_create_display(){
 	static lv_display_t *disp;
 	static uint8_t draw_buf[250*122];
-	disp = lv_display_create(250,122);
+	disp = lv_display_create(122,250);
 	lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
 	lv_display_set_flush_cb(disp, epd_hal_flush);
 	lv_display_set_color_format(disp, LV_COLOR_FORMAT_I1);
