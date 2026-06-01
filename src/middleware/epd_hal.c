d0692f90731e55f4cd019c633d92cfeab883da42
utkarsharora100 <utkarsharora100@gmail.com>
2026-06-02 01:03:01 +0530

    The example button works git add . hurray

--- src/middleware/epd_hal.c
+++ src/middleware/epd_hal.c
@@ -34,26 +34,24 @@ waveshare needs 1 bit per pixel , 8 pixels are packed into 1 bytes */
 
 /* 250 pixels / 8 pixels per byte = 31.25 bytes that is 32 */
 /* Eink uses MSB therefore we start from 7th index of the byte sequence */
+/*BIG WARNING !!!! This code only works for lvgl 9+ because the starting byte is the pallete header here*/
 void epd_hal_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 {
-    int stride = 32; // LVGL: 250px wide at I1 = ceil(250/8) = 32 bytes per row
-    printf("flush: x1=%d y1=%d x2=%d y2=%d\n", area->x1, area->y1, area->x2, area->y2);
+    px_map += 8;
+    int area_w = area->x2 - area->x1 + 1;
+    int stride = (area_w + 7) / 8;
+
     for (int y = area->y1; y <= area->y2; y++) {
         int local_y = y - area->y1;
         for (int x = area->x1; x <= area->x2; x++) {
-            if (x >= 250) continue;  // skip LVGL padding (stride=32=256bits, valid=250)
-            int local_x  = x - area->x1;
+            if (x >= 250) continue;
+            int local_x = x - area->x1;
 
-            // read from LVGL buffer (row-major, stride=32)
             int src_byte = local_y * stride + local_x / 8;
             int src_bit  = 7 - (local_x % 8);
             int pixel    = (px_map[src_byte] >> src_bit) & 1;
 
-            // TRANSPOSE: LVGL x→EPD Y_row (j), LVGL y→EPD X_byte (i) + bit
-            // Image[i + j*16] → EPD (X_byte=i, Y_row=j)
-            // We want: LVGL (x,y) → physical (x_phys=x, y_phys=y)
-            // So: j = x (Y_row = LVGL_x), i = y/8 (X_byte = LVGL_y/8)
-            int dst_byte = (249-x) * 16 + y / 8;
+            int dst_byte = (249 - x) * 16 + y / 8;
             int dst_bit  = 7 - (y % 8);
 
             if (pixel)
@@ -64,17 +62,17 @@ void epd_hal_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
     }
 
     if (lv_display_flush_is_last(disp)) {
+        memcpy(epd_buf, stage_buf, sizeof(epd_buf));
         partial_refresh_counter++;
-	memcpy(epd_buf, stage_buf, sizeof(epd_buf));
         if (partial_refresh_counter >= 5) {
             EPD_2in13_V4_Init(EPD_2IN13_V4_FULL);
             EPD_2in13_V4_Display_Base(epd_buf);
-            EPD_2in13_V4_Init(EPD_2IN13_V4_PART); 
+            EPD_2in13_V4_Init(EPD_2IN13_V4_PART);
             EPD_2in13_V4_Display_Partial_Wait(epd_buf);
             EPD_2in13_V4_Sleep();
             partial_refresh_counter = 0;
         } else {
-            //EPD_2in13_V4_Init(EPD_2IN13_V4_PART);
+            EPD_2in13_V4_Init(EPD_2IN13_V4_PART);
             EPD_2in13_V4_Display_Partial_Wait(epd_buf);
             EPD_2in13_V4_Sleep();
         }
