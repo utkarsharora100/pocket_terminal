bc7ff4d8b33948d141fb0295a27a25877bc2d6d2
utkarsharora100 <166918728+utkarsharora100@users.noreply.github.com>
2026-06-12 17:15:26 +0530

    uploaded files from correct directory

---
+++ src/ui/vars.h
@@ -0,0 +1,27 @@
+#ifndef EEZ_LVGL_UI_VARS_H
+#define EEZ_LVGL_UI_VARS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// enum declarations
+
+// Flow global variables
+
+enum FlowGlobalVariables {
+    FLOW_GLOBAL_VARIABLE_SELECTED = 0,
+    FLOW_GLOBAL_VARIABLE_TYPED = 1,
+    FLOW_GLOBAL_VARIABLE_TERMINAL_BUFFER = 2
+};
+
+// Native global variables
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_VARS_H*/
